8eb70caed575d2e0c6b99f6469bf28525df6095a
patrickgu31 <patrick.gu31@gmail.com>
2025-10-28 20:12:26 +1100

    stuff

--- src/main/kotlin/com/UrlShortenerController.kt
+++ src/main/kotlin/com/UrlShortenerController.kt
@@ -1,23 +1,39 @@
 package com
 
 import com.service.UrlShortenerService
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.PathVariable
 import jakarta.inject.Inject
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.net.URI
 
 @Controller("/urlshortener")
 class UrlShortenerController (
-    @Inject private val urlShortenerService: UrlShortenerService
+    @Inject private val urlShortenerService: UrlShortenerService,
+    @Value("\${url.shortener.prefix}") private val prefix: String
 ){
-
+    companion object{
+        private val logger: Logger = LoggerFactory.getLogger(UrlShortenerController::class.java)
+    }
     @Get("/hello")
     fun hello(): String = "Hello from Micronaut!"
 
     @Get("/shorten/{url}")
     fun shorten(@PathVariable url: String): HttpResponse<Any>? {
-        urlShortenerService.shortenUrl(url)
-        return HttpResponse.ok(url)
+        logger.info("shortening: $url")
+        try  {
+            val test = URI.create(url)
+            test.toURL()
+        } catch (e: IllegalArgumentException){
+            return HttpResponse.badRequest("Invalid URL")
+        }
+        val urlToEncode = URI.create(url)
+        val response = urlShortenerService.shortenUrl(urlToEncode)
+        return HttpResponse.ok(prefix)
     }
 }
--- src/main/kotlin/com/service/UrlShortenerService.kt
+++ src/main/kotlin/com/service/UrlShortenerService.kt
@@ -1,10 +1,21 @@
 package com.service
 
 import jakarta.inject.Singleton
+import java.net.URI
 
 @Singleton
 class UrlShortenerService {
-    fun shortenUrl(url: String): String {
-        return url
+    fun shortenUrl(url: URI): String {
+        if (url.scheme != "http" && url.scheme != "https") {
+            throw IllegalArgumentException("Only Http/Https URL are supported")
+        }
+        return url.toString()
+    }
+
+    fun generateUniqueRandomKey() {
+
+    }
+
+    fun generateRandomKey(){
     }
 }
